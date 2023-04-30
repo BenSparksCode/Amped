696957ab8dca64c265367c8f069263f2140e45d3
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-04-30 15:20:43 +0200

    Add auth imports, sign out logic, formatting

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,27 +6,45 @@ import {
   TextInput,
   Pressable,
   SafeAreaView,
-} from 'react-native';
-import {API, graphqlOperation} from 'aws-amplify';
-import {createTodo} from './src/graphql/mutations';
-import {listTodos} from './src/graphql/queries';
+} from "react-native";
+import { API, graphqlOperation } from "aws-amplify";
+import { createTodo } from "./src/graphql/mutations";
+import { listTodos } from "./src/graphql/queries";
 
-import { Amplify } from 'aws-amplify';
-import awsExports from './src/aws-exports';
+import { Amplify } from "aws-amplify";
+import awsExports from "./src/aws-exports";
+import {
+  withAuthenticator,
+  useAuthenticator,
+} from "@aws-amplify/ui-react-native";
+
 Amplify.configure(awsExports);
 
-const initialState = {name: '', description: ''};
+const initialState = { name: "", description: "" };
 
 const App = () => {
   const [formState, setFormState] = useState(initialState);
   const [todos, setTodos] = useState([]);
+  // retrieves only the current value of 'user' from 'useAuthenticator'
+  const userSelector = (context) => [context.user];
 
   useEffect(() => {
     fetchTodos();
   }, []);
 
+  const SignOutButton = () => {
+    const { user, signOut } = useAuthenticator(userSelector);
+    return (
+      <Pressable onPress={signOut} style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>
+          Hello, {user.username}! Click here to sign out!
+        </Text>
+      </Pressable>
+    );
+  };
+
   function setInput(key, value) {
-    setFormState({...formState, [key]: value});
+    setFormState({ ...formState, [key]: value });
   }
 
   async function fetchTodos() {
@@ -35,19 +53,19 @@ const App = () => {
       const todos = todoData.data.listTodos.items;
       setTodos(todos);
     } catch (err) {
-      console.log('error fetching todos');
+      console.log("error fetching todos");
     }
   }
 
   async function addTodo() {
     try {
       if (!formState.name || !formState.description) return;
-      const todo = {...formState};
+      const todo = { ...formState };
       setTodos([...todos, todo]);
       setFormState(initialState);
-      await API.graphql(graphqlOperation(createTodo, {input: todo}));
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
     } catch (err) {
-      console.log('error creating todo:', err);
+      console.log("error creating todo:", err);
     }
   }
 
@@ -55,13 +73,13 @@ const App = () => {
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <TextInput
-          onChangeText={value => setInput('name', value)}
+          onChangeText={(value) => setInput("name", value)}
           style={styles.input}
           value={formState.name}
           placeholder="Name"
         />
         <TextInput
-          onChangeText={value => setInput('description', value)}
+          onChangeText={(value) => setInput("description", value)}
           style={styles.input}
           value={formState.description}
           placeholder="Description"
@@ -83,14 +101,19 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
-  container: {width: 400, flex: 1, padding: 20, alignSelf: 'center'},
-  todo: {marginBottom: 15},
-  input: {backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18},
-  todoName: {fontSize: 20, fontWeight: 'bold'},
+  container: { width: 400, flex: 1, padding: 20, alignSelf: "center" },
+  todo: { marginBottom: 15 },
+  input: {
+    backgroundColor: "#ddd",
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: "bold" },
   buttonContainer: {
-    alignSelf: 'center',
-    backgroundColor: 'black',
+    alignSelf: "center",
+    backgroundColor: "black",
     paddingHorizontal: 8,
   },
-  buttonText: {color: 'white', padding: 16, fontSize: 18},
+  buttonText: { color: "white", padding: 16, fontSize: 18 },
 });
